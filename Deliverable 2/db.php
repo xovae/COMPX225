73e7bf95da71f8bd855ecc05252ddc5ad7b70344
xovae <89520672+xovae@users.noreply.github.com>
2025-09-17 14:59:18 +1200

    Initial Commit

---
+++ Deliverable 2/db.php
@@ -0,0 +1,8 @@
+<?php
+    $con = mysqli_connect("webapp.cms.waikato.ac.nz",
+    "jb469", "my301216sql", "jb469");
+
+    if ($con == false) {
+        die("ERROR");
+    }
+?>
---
+++ Deliverable 2/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kiwi Kloset</title>
+</head>
+<body>
+    <!-- navbar w/ logo? -->
+    <h1>Kiwi Kloset</h1>
+
+    <!-- instructions -->
+
+    <!-- table of all costumes -->
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Is Available</th> <!-- Make it so 0 and 1 are formatted to y/n? -->
+            <th>Branch ID</th>
+            <th>Name</th>
+            <th>Size</th>
+            <th>Daily Rate</th>
+            <th>Category</th>
+        </tr>
+
+        <?php
+            require_once("db.php");
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['is_available'] . "</td>";
+                echo "<td>" . $row['branch_id'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['size'] . "</td>";
+                echo "<td>" . $row['daily_rate'] . "</td>";
+                echo "<td>" . $row['category'] . "</td>";
+                echo "</tr>";
+            };
+
+        ?>
+
+    </table>
+
+    <!-- form that takes a costume id, sends a GET to rentals.php -->
+
+    <!-- form that takes new costume info, sends a POST to add.php -->
+</body>
+</html>
