8e694ca6cee5241eed8eb282669aeb1f1efe69ce
Jamie Boulton <jamie.william.boulton@gmail.com>
2025-09-18 20:05:52 +1200

    CSS changes

--- Deliverable 2/index.php
+++ Deliverable 2/index.php
@@ -60,7 +60,7 @@
                 ?>
             </table>
         </div>
-        <div id="forms">
+        <div>
             <h2>Instructions</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae nemo ab magnam? Temporibus explicabo sed incidunt et dolores earum, eos reiciendis, aperiam nobis dolorum ipsam distinctio fugiat similique? Vero, ab.
             Repellendus aliquid, dolorem atque minima, nisi nam labore numquam molestiae rem libero distinctio facilis dolor ex totam perferendis quos pariatur fuga quia illum quaerat voluptatem ratione qui dolores. Quam, libero.</p>
@@ -68,63 +68,54 @@
             <h2>Get Costume By ID</h2>
 
             <form action="rentals.php" method="GET">
-                <div>
-                    <label for="costume_id">Costume ID</label>
-                    <input type="number" name="costume_id" id="costume_id" placeholder="Costume ID" min="0" required>
-                </div>
-
-                <div>
-                    <button type="submit" class="buttons">Submit</button>
-                    <button type="reset" class="buttons">Reset</button>
-                </div>
+                <label for="costume_id">Costume ID</label>
+                <input type="number" name="costume_id" id="costume_id" placeholder="Costume ID" min="0" required>
+                <button type="submit">Submit</button>
+                <button type="reset">Reset</button>
             </form>
 
             <h2>Add Costume</h2>
 
-            <form action="add.php" method="POST">
-                <div>
-                    <label for="new_costume_id">Costume ID</label>
-                    <input type="number" name="new_costume_id" id="new_costume_id" required>
+            <form id="add" action="add.php" method="POST">
+                <label for="new_costume_id">Costume ID</label>
+                <input type="number" name="new_costume_id" id="new_costume_id" required>
 
-                    <label for="is_available">Is Available</label>
-                    <select name="is_available" id="is_available" required>
-                        <option value="1">Yes</option>
-                        <option value="0">No</option>
-                    </select>
+                <label for="is_available">Is Available</label>
+                <select name="is_available" id="is_available" required>
+                    <option value="1">Yes</option>
+                    <option value="0">No</option>
+                </select>
 
-                    <label for="branch_id">Branch ID</label>
-                    <input type="number" name="branch_id" id="branch_id" min="0" required>
+                <label for="branch_id">Branch ID</label>
+                <input type="number" name="branch_id" id="branch_id" min="0" required>
 
-                    <label for="costume_name">Name</label>
-                    <input type="text" name="costume_name" id="costume_name" required>
+                <label for="costume_name">Name</label>
+                <input type="text" name="costume_name" id="costume_name" required>
 
-                    <label for="size">Size</label>
-                    <select name="size" id="size" required>
-                        <option value="Mens Extra Small">Mens Small</option>
-                        <option value="Mens Small">Mens Small</option>
-                        <option value="Mens Medium">Mens Medium</option>
-                        <option value="Mens Large">Mens Large</option>
-                        <option value="Mens Extra Large">Mens Extra Large</option>
-                        <option value="Womans Extra Small">Womans Extra Small</option>
-                        <option value="Womans Small">Womans Small</option>
-                        <option value="Womans Medium">Womans Medium</option>
-                        <option value="Womans Large">Womans Large</option>
-                        <option value="Womans Extra Large">Womans Extra Large</option>
-                        <option value="Standard">Standard</option>
-                    </select>
+                <label for="size">Size</label>
+                <select name="size" id="size" required>
+                    <option value="Mens Extra Small">Mens Small</option>
+                    <option value="Mens Small">Mens Small</option>
+                    <option value="Mens Medium">Mens Medium</option>
+                    <option value="Mens Large">Mens Large</option>
+                    <option value="Mens Extra Large">Mens Extra Large</option>
+                    <option value="Womans Extra Small">Womans Extra Small</option>
+                    <option value="Womans Small">Womans Small</option>
+                    <option value="Womans Medium">Womans Medium</option>
+                    <option value="Womans Large">Womans Large</option>
+                    <option value="Womans Extra Large">Womans Extra Large</option>
+                    <option value="Standard">Standard</option>
+                </select>
 
-                    <label for="daily_rate">Daily Rate</label>
-                    <input type="number" name="daily_rate" id="daily_rate" step="0.01" min="0" required>
+                <label for="daily_rate">Daily Rate</label>
+                <input type="number" name="daily_rate" id="daily_rate" step="0.01" min="0" required>
 
-                    <!-- Maybe a dropdown? -->
-                    <label for="category">Category</label>
-                    <input type="text" name="category" id="category" required>
-                </div>
+                <!-- Maybe a dropdown? -->
+                <label for="category">Category</label>
+                <input type="text" name="category" id="category" required>
 
-                <div>
-                    <button type="submit" class="buttons">Submit</button>
-                    <button type="reset" class="buttons">Reset</button>
-                </div>
+                <button type="submit">Submit</button>
+                <button type="reset">Reset</button>
             </form>
         </div>
     </div>
